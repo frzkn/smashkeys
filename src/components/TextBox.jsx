253e364c96725bfb2a1329993bff5c398525dd1d
Faraz Khan <farazrk001@gmail.com>
2020-05-13 18:40:13 +0530

    Improvements

--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -4,7 +4,7 @@ const TextBox = ({ inputText, setInputText, startGame, disable, seconds }) => {
   return (
     <div className="flex items-center justify-center flex-auto">
       <input
-        className="flex-grow w-full p-2 px-4 mx-2 text-2xl border-2 outline-purple text-grey-darkest"
+        className="flex-grow w-full p-2 px-4 mx-2 text-2xl border-2 rounded outline-purple text-grey-darkest"
         type="text"
         placeholder=""
         value={inputText}
@@ -14,7 +14,7 @@ const TextBox = ({ inputText, setInputText, startGame, disable, seconds }) => {
         }}
         disabled={disable ? true : false}
       />
-      <div className="flex items-center justify-center p-3 text-xl text-center bg-white rounded-lg text-grey-darkest border-purple-light ">
+      <div className="flex items-center justify-center p-3 text-xl text-center bg-white rounded-lg text-grey-darkest border-purple-light">
         <span className="w-4 text-grey-darkest">{seconds} </span>
       </div>
     </div>
